268f541379d4023255dee0c316fdf684413a32ea
Chrisando Ryan <chrisando.siahaan@gmail.com>
2018-11-04 12:23:18 +0700

    added cart modal

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -2,21 +2,40 @@ import React, { Component, Fragment } from 'react'
 import {
     Link
 } from 'react-router-dom'
+import Modal from 'react-modal';
 import Header from '../shared/Header';
 import Footer from '../shared/Footer';
 import DisplayPromotedProducts from '../products/DisplayPromotedProducts';
 import PopularTagService from '../../services/PopularTagService';
+import CartService from '../../services/CartService';
 // import '../../css/lapak.css';
 // import '../../css/helep.css';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
 
 class Dashboard extends Component {
     constructor(props) {
         super(props);
         console.log(props);
         this.state = {
-            populars: []
+            populars: [],
+            cartContent: [],
+            modalIsOpen: false
         }
         this.popularService = new PopularTagService();
+        this.cart = new CartService();
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     componentDidMount() {
@@ -27,13 +46,35 @@ class Dashboard extends Component {
             .catch(err => {
                 alert(err.message);
             })
+        this.cart.getCart()
+            .then(res => {
+                const cartContent = res.data.data;
+                this.setState({ cartContent });
+                if (cartContent.length > 0) this.setState({modalIsOpen: true})
+            })
+            .catch(err => {
+                // alert(err.message);
+            });
+    }
+
+    openModal() {
+        this.setState({ modalIsOpen: true });
+    }
+
+    afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        this.subtitle.style.color = '#f00';
+    }
+
+    closeModal() {
+        this.setState({ modalIsOpen: false });
     }
 
     render() {
         return (
             <div>
                 <div className="app">
-                    <Header {...this.props}/>
+                    <Header {...this.props} />
                     <div>
                         <div className="home-banner featured-main">
                             <div className="container-grid">
@@ -82,6 +123,33 @@ class Dashboard extends Component {
                                 </div>
                             </div>
                         </div>
+                        {/* <button onClick={this.openModal}>Open Modal</button> */}
+                        <Modal
+                            isOpen={this.state.modalIsOpen}
+                            onAfterOpen={this.afterOpenModal}
+                            onRequestClose={this.closeModal}
+                            style={customStyles}
+                            contentLabel="Whoops!"
+                        >
+                            <h2 ref={subtitle => this.subtitle = subtitle}>Whoops! Jangan lupa teruskan belanja anda!</h2>
+                            {
+                                this.state.cartContent.map((data, index) => {
+                                    return (
+                                        <div className="product">
+                                            <div className="product-image">
+                                                <img src="https://via.placeholder.com/300x300" />
+                                            </div>
+                                            <div className="product-details">
+                                                <div className="product-title" style={{margin: 15}}>{data.product.name}</div>
+                                                {/* <p className="product-description">The best dog bones of all time. Holy crap. Your dog will be begging for these things! I got curious once and ate one myself. I'm a fan.</p> */}
+                                            </div>
+                                            <div className="product-price" style={{margin: 5}}>{data.product.price}</div>
+                                        </div>
+                                    )
+                                })
+                            }
+                            <button onClick={this.closeModal}>close</button>
+                        </Modal>
                         <DisplayPromotedProducts />
                     </div>
                     <Footer />
